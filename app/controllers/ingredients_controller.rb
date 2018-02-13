98d49366ec84d05ff8a50a46866bbde0e27bac5f
Keshden <bleonrey7@gmail.com>
2018-02-12 20:54:05 -0700

    Complete Ingredients resource

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,12 +1,19 @@
 class IngredientsController < ApplicationController
   before_action :set_ingredient, only: [:edit, :update, :show]
+  before_action :require_admin, except: [:show, :index]
 
   def new
-
+    @ingredient = Ingredient.new
   end
 
   def create
-
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      flash[:success] = "Ingredient successfully created"
+      redirect_to ingredient_path(@ingredient)
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -14,7 +21,12 @@ class IngredientsController < ApplicationController
   end
 
   def update
-
+    if @ingredient.update(ingredient_params)
+      flash[:success] = "Ingredient updated successfully"
+      redirect_to @ingredient
+    else
+      render 'edit'
+    end
   end
 
   def show
@@ -27,12 +39,19 @@ class IngredientsController < ApplicationController
 
   private
 
-  #def ingredient_params
-  #  params.require[:ingredient].permit[:]
-  #end
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
 
   def set_ingredient
     @ingredient = Ingredient.find(params[:id])
+  end
+
+  def require_admin
+    if !logged_in? || (logged_in? && !current_chef.admin?)
+      flash[:danger] = "Only admin users can perform that action"
+      redirect_to ingredients_path
+    end
   end
 
 
